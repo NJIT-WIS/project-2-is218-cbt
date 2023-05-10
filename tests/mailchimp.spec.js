8569235400265f869744720ea65bc2877f182285
Truong Dang <ddtblock1903@gmail.com>
2023-05-10 17:58:03 -0400

    truog update mailchimp test

--- tests/mailchimp.spec.js
+++ tests/mailchimp.spec.js
@@ -1,18 +1,18 @@
-const { test, expect } = require('@playwright/test')
-const { chromium } = require('playwright')
-const path = require('path')
+const { test, expect } = require('@playwright/test');
+const { chromium } = require('playwright');
+const path = require('path');
 
-const config = require(path.join(process.cwd(), 'playwright.config.js'))
-const { pages } = require(path.join(process.cwd(), 'tests', 'pages.json'))
+const config = require(path.join(process.cwd(), 'playwright.config.js'));
+const { pages } = require(path.join(process.cwd(), 'tests', 'pages.json'));
 
-const TIMEOUT = 30000
+const TIMEOUT = 30000;
 
 async function checkEmailInputElement (pageUrl) {
-    const browser = await chromium.launch()
-    const page = await browser.newPage()
-    await page.goto(pageUrl, { timeout: TIMEOUT })
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+    await page.goto(pageUrl, { timeout: TIMEOUT });
 
-    const emailInput = await page.$('input[placeholder="Enter your email"]')
+    const emailInput = await page.$('mce-EMAIL');
 
     if (emailInput) {
         const outerHTML = await emailInput.evaluate((el) => el.outerHTML)
