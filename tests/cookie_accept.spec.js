db11a7720a72fa6b23d0b614cc2c73a5ec0f82c9
Truong Dang <94695575+2dtkingslayer@users.noreply.github.com>
2023-05-10 03:14:02 -0400

    Delete cookie_accept.spec.js

--- tests/cookie_accept.spec.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-const { test, expect } = require('@playwright/test')
-
-test('Test Cookie Accept', async ({ page }) => {
-    await page.goto('http://localhost:3000')
-    await page.waitForSelector('.cookie-modal', { state: 'hidden' })
-    expect(await page.isVisible('.cookie-modal')).toBe(false)
-})
