1386471ffea7c90a5335fbbaed719b0ffb724e83
Truong Dang <94695575+2dtkingslayer@users.noreply.github.com>
2023-05-10 02:48:48 -0400

    Update $ sign

--- tests/title_h_tag.spec.js
+++ tests/title_h_tag.spec.js
@@ -3,6 +3,6 @@ const { test, expect } = require('@playwright/test')
 
 test('Test H2 Tag', async ({ page }) => {
     await page.goto('http://localhost:3000')
-    const h2Tag = await page.$('h2')
+    const h2Tag = await page.$$('h2')
     expect(h2Tag).not.toBeNull()
 })
