da61685113e5c19c3c90201a9c7805e5f26d1d24
Truong Dang <94695575+2dtkingslayer@users.noreply.github.com>
2023-05-10 03:54:56 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ export default function Home({ allPostsData }) {
                         <div className={bodyUI.rightcolumn}>
                             <div className={bodyUI.card} style={{ justifyContent:'center'}}>
                                 <h2 className={bodyUI.rightSideTitle} style={{textAlign: 'center'}}>About Us</h2>
-                                <img className={bodyUI.Logo} src="/images/profile.jpg" style={{height: '100px'}} alt={'aboutusimage'}/>
+                                <img className={bodyUI.Logo} src="images/profile.jpg" style={{height: '100px'}} alt='aboutusimage'/>
                                 <p style={{textAlign: 'justify'}}>{siteTitle} is built by {name} that stands for Callie, Bryan, and Truong. We empower teachers and students with web development skill and AI knowledge.</p>
                             </div>
                             <div className={bodyUI.card}>
