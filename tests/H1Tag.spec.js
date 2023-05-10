1e5a09504b05ef6cf849df2d8fcb4e046a1c4b8b
Truong Dang <ddtblock1903@gmail.com>
2023-05-10 18:42:08 -0400

    Truong h1 tag test

---
+++ tests/H1Tag.spec.js
@@ -0,0 +1,24 @@
+const { test, expect } = require('@playwright/test');
+const { chromium } = require('playwright');
+const path = require('path');
+
+const config = require(path.join(process.cwd(), 'playwright.config.js'));
+const { pages } = require(path.join(process.cwd(), 'tests', 'pages.json'));
+
+const TIMEOUT = 30000;
+async function checkH1(pageUrl) {
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+    await page.goto(pageUrl, { timeout: TIMEOUT });
+    const h1Elements = await page.$$('h1');
+    await browser.close();
+    return h1Elements.length > 0;
+}
+
+pages.forEach((page) => {
+    test(Page "${page.path}" should have at least one h1 tag, async ({}) => {
+        const pageUrl = ${config.use.baseURL}${page.path};
+        const hasH1Tag = await checkH1T(pageUrl);
+        expect(hasH1Tag).toBeTruthy();
+    });
+});
